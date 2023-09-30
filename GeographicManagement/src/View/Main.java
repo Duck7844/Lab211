dafc3a03aeb958dd61f547726579d42c08422566
Duck-is-coding <144187637+Duck7844@users.noreply.github.com>
2023-09-30 19:32:57 +0700

    Add files via upload

---
+++ GeographicManagement/src/View/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package View;
+
+import Controller.Controller;
+/**
+ *
+ * @author ROG
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        new Controller().run();
+    }
+    
+}
