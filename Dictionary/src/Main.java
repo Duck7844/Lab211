1be37ceb3d7c230d9e42711b1ddb28ffba55d830
Duck-is-coding <144187637+Duck7844@users.noreply.github.com>
2023-10-16 11:14:42 +0700

    Add files via upload

---
+++ Dictionary/src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import Controller.Controller;
+/**
+ *
+ * @author ROG
+ */
+public class Main
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        new Controller().run();
+    }
+    
+}
